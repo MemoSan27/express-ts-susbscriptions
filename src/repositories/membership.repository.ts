27375e71b6557acb91c72a0cc3972ba2a3d6de21
memo <memo2705@gmail.com>
2024-03-28 07:37:08 -0700

    membership repository finished

---
+++ src/repositories/membership.repository.ts
@@ -0,0 +1,94 @@
+import { Db, Filter, ObjectId } from "mongodb";
+import dbConnection from "../configs/database/mongo.conn";
+import { Membership } from "../models/Membership";
+
+export const getAllMembershipsRepository = async (): Promise<Membership[] | null> => {
+    try {
+        const db: Db = await dbConnection();
+        const memberships = await db.collection<Membership>('memberships').find().toArray();
+
+        return memberships; 
+    } catch (error) {
+        console.error('Error getting memberships: ', error);
+        return null;
+    }
+};
+
+export const createMembershipRepository = async (
+    membership: Membership
+): Promise<ObjectId | null> => {
+    try {
+        const db: Db = await dbConnection(); 
+        const result = await db.collection<Membership>('memberships').insertOne(membership);
+
+        return result.insertedId ? new ObjectId(result.insertedId) : null;
+    } catch (error) {
+        console.error('Error creating membership: ', error);
+        return null;
+    }
+};
+
+export const getMembershipByIdRepository = async (
+    membershipId: string
+): Promise<Membership | null> => {
+    try {
+        const db: Db = await dbConnection();
+
+        if (!ObjectId.isValid(membershipId)) {
+            throw new Error('Invalid membership ID');
+        }
+
+        const filter = { _id: new ObjectId(membershipId) };
+        const membership = await db.collection<Membership>('memberships').findOne(filter);
+
+        return membership; 
+    } catch (error) {
+        console.error('Error getting membership by ID: ', error);
+        return null;
+    }
+};
+
+export const deleteMembershipByIdRepository = async (
+    membershipId: string
+): Promise<boolean> => {
+    try {
+        const db: Db = await dbConnection();
+
+        if (!ObjectId.isValid(membershipId)) {
+            throw new Error('Invalid membership ID');
+        }
+
+        const filter = { _id: new ObjectId(membershipId) };
+        const result = await db.collection('memberships').deleteOne(filter);
+
+        return result.deletedCount === 1;
+    } catch (error) {
+        console.error('Error deleting membership:', error);
+        return false;
+    }
+};
+
+export const updateMembershipRepository = async (
+    membershipId: string, 
+    updatedMembershipData: Partial<Membership>
+): Promise<boolean> => {
+    try {
+        const db: Db = await dbConnection(); 
+        
+        if (!ObjectId.isValid(membershipId)) {
+            throw new Error('Invalid membership ID');
+        }
+        
+        const filter: Filter<Membership> = { _id: new ObjectId(membershipId) }; 
+        
+        const result = await db.collection<Membership>('memberships').updateOne(
+            filter, 
+            { $set: updatedMembershipData } 
+        );
+
+        return result.modifiedCount === 1; 
+    } catch (error) {
+        console.error('Error updating membership: ', error);
+        return false;
+    }
+};
--- src/services/membership.service.ts
+++ src/services/membership.service.ts
@@ -1,98 +1,61 @@
 import { Db, ObjectId, Filter } from 'mongodb';
 import dbConnection from '../configs/database/mongo.conn';
 import { Membership } from '../models/Membership';
+import { createMembershipRepository, deleteMembershipByIdRepository, getAllMembershipsRepository, getMembershipByIdRepository, updateMembershipRepository } from '../repositories/membership.repository';
 
 //Get all memberships info service
-export const getAllMembershipsService = async(): Promise<Membership[] | null> => {
+export const getAllMembershipsService = async (): Promise<Membership[] | null> => {
     try {
-        const db: Db = await dbConnection(); 
-        
-        const memberships = await db.collection<Membership>('memberships').find().toArray();
-        return memberships; 
+        return await getAllMembershipsRepository();
     } catch (error) {
         console.error('Error getting memberships: ', error);
         return null;
     }
-}
-
+};
 
 //Create new membership service
-export const createMembershipService = async(
+export const createMembershipService = async (
     membership: Membership
-    ): Promise<ObjectId | null> => {
+): Promise<ObjectId | null> => {
     try {
-        const db: Db = await dbConnection(); 
-        const result = await db.collection<Membership>('memberships').insertOne(membership);
-
-        return result.insertedId ? new ObjectId(result.insertedId) : null;
+        return await createMembershipRepository(membership);
     } catch (error) {
         console.error('Error creating membership: ', error);
         return null;
     }
-}
+};
 
 //Get a single membership by id service
-export const getMembershipByIdService = async(
+export const getMembershipByIdService = async (
     membershipId: string
-    ): Promise<Membership | null> => {
+): Promise<Membership | null> => {
     try {
-        const db: Db = await dbConnection();
-
-        if (!ObjectId.isValid(membershipId)) {
-            throw new Error('Invalid membership ID');
-        }
-
-        const filter = { _id: new ObjectId(membershipId) };
-        const membership = await db.collection<Membership>('memberships').findOne(filter);
-
-        return membership; 
+        return await getMembershipByIdRepository(membershipId);
     } catch (error) {
         console.error('Error getting membership by ID: ', error);
         return null;
     }
-}
+};
 
 //Delete a membership service
-export const deleteMembershipByIdService = async(
+export const deleteMembershipByIdService = async (
     membershipId: string
-    ): Promise<boolean> => {
+): Promise<boolean> => {
     try {
-        const db: Db = await dbConnection();
-
-        if (!ObjectId.isValid(membershipId)) {
-            throw new Error('Invalid membership ID');
-        }
-
-        const filter = { _id: new ObjectId(membershipId) };
-        const result = await db.collection('memberships').deleteOne(filter);
-
-        return result.deletedCount === 1;
+        return await deleteMembershipByIdRepository(membershipId);
     } catch (error) {
         console.error('Error deleting membership:', error);
         return false;
     }
-}
+};
 
 //Update membership service
-export const updateMembershipService = async(
+export const updateMembershipService = async (
     membershipId: string, 
     updatedMembershipData: Partial<Membership>
-    ): Promise<boolean> => {
+): Promise<boolean> => {
     try {
-        const db: Db = await dbConnection(); 
-        
-        if (!ObjectId.isValid(membershipId)) {
-            throw new Error('Invalid membership ID');
-        }
-        
-        const filter: Filter<Membership> = { _id: new ObjectId(membershipId) }; 
-        
-        const result = await db.collection<Membership>('memberships').updateOne(
-            filter, 
-            { $set: updatedMembershipData } 
-        );
-
-        return result.modifiedCount === 1; 
+        return await updateMembershipRepository(membershipId, updatedMembershipData);
     } catch (error) {
         console.error('Error updating membership: ', error);
         return false;
