858cea00b50ed107f58cdd3b524e9f39933f8e3f
memo <memo2705@gmail.com>
2024-03-28 07:21:59 -0700

    game repository finished

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -3,6 +3,7 @@ import dbConnection from '../configs/database/mongo.conn';
 import { Game } from '../models/Game';
 import { Membership } from '../models/Membership';
 import cache from '../middlewares/cache/nodeCacheInstance';
+import { createGameRepository, deleteGameByIdRepository, getAllGamesRepository, getGameByIdRepository, updateGameRepository } from '../repositories/game.repository';
 
 // Get all games service chached for 5 minutes
 export const getAllGamesService = async (): Promise<Game[] | null> => {
@@ -14,11 +15,11 @@ export const getAllGamesService = async (): Promise<Game[] | null> => {
             return cachedGames;
         }
         
-        const db: Db = await dbConnection();
-        const games = await db.collection<Game>('games').find().toArray();
+        const games = await getAllGamesRepository();
         
-        
-        cache.set('allGames', games, 300);
+        if (games !== null) {
+            cache.set('allGames', games, 300);
+        }
 
         return games; 
     } catch (error) {
@@ -28,32 +29,15 @@ export const getAllGamesService = async (): Promise<Game[] | null> => {
 };
 
 // Create a new game service
-export const createGameService = async (
-    game: Game
-): Promise<ObjectId | null> => {
+export const createGameService = async (game: Game): Promise<ObjectId | null> => {
     try {
-        const db: Db = await dbConnection(); 
-    
-        const membershipsCollection = db.collection<Membership>('memberships');
-        const filter = { _id: new ObjectId(game.membershipRequiredId) };
-        const existingMembership = await membershipsCollection.findOne(filter);
+        const result = await createGameRepository(game);
 
-        if (!existingMembership) {
-            throw new Error(`Membership with id: ${game.membershipRequiredId} does not exist.`);
+        if (result !== null) {
+            cache.del('allGames');
         }
 
-        const gamesCollection = db.collection<Game>('games');
-        const existingGame = await gamesCollection.findOne({ title: game.title });
-        
-        if (existingGame) {
-            return null; 
-        }
-
-        const result = await gamesCollection.insertOne(game);
-
-        cache.del('allGames');
-
-        return result.insertedId ? new ObjectId(result.insertedId) : null;
+        return result;
     } catch (error) {
         console.error('Error creating this game: ', error);
         return null;
@@ -61,74 +45,45 @@ export const createGameService = async (
 };
 
 // Get a single game by its ID service
-export const getGameByIdService = async(gameId: string): Promise<Game | null> => {
+export const getGameByIdService = async (gameId: string): Promise<Game | null> => {
     try {
-        const db: Db = await dbConnection();
-
-        if (!ObjectId.isValid(gameId)) {
-            throw new Error('Invalid game ID');
-        }
-
-        const filter = { _id: new ObjectId(gameId) };
-        const game = await db.collection<Game>('games').findOne(filter);
-
-        return game; 
+        return await getGameByIdRepository(gameId);
     } catch (error) {
         console.error('Error getting game by ID: ', error);
         return null;
     }
-}
+};
 
 // Delete a game by ID service
-export const deleteGameByIdService = async(gameId: string): Promise<boolean> => {
+export const deleteGameByIdService = async (gameId: string): Promise<boolean> => {
     try {
-        const db: Db = await dbConnection();
+        const result = await deleteGameByIdRepository(gameId);
 
-        if (!ObjectId.isValid(gameId)) {
-            throw new Error('Invalid game ID');
-        }
-
-        const filter = { _id: new ObjectId(gameId) };
-        const result = await db.collection<Game>('games').deleteOne(filter);
-
-        if (result.deletedCount === 1) {
+        if (result) {
             cache.del('allGames');
-            return true;
-        } else {
-            return false;
         }
+
+        return result;
     } catch (error) {
         console.error('Error deleting game:', error);
         return false;
     }
-}
+};
 
 
 // Update a game by its ID service
-export const updateGameService = async(
+export const updateGameService = async (
     gameId: string, 
     updatedGameData: Partial<Game>
-    ): Promise<boolean> => {
+): Promise<boolean> => {
     try {
-        const db: Db = await dbConnection(); 
-        
-        if (!ObjectId.isValid(gameId)) {
-            throw new Error('Invalid game ID');
-        }
-        
-        const filter: Filter<Game> = { _id: new ObjectId(gameId) }; 
-        
-        const result = await db.collection<Game>('games').updateOne(
-            filter, 
-            { $set: updatedGameData } 
-        );
+        const result = await updateGameRepository(gameId, updatedGameData);
 
-        if (result.modifiedCount === 1) {
+        if (result) {
             cache.del('allGames');
-            return true;
-        } else {
-            return false;
         }
+
+        return result;
     } catch (error) {
         console.error('Error updating game: ', error);
         return false;
