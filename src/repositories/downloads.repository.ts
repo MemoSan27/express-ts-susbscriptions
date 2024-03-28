5b2b05ba7096e304a30febafc1cb53b3da726e21
memo <memo2705@gmail.com>
2024-03-28 08:47:26 -0700

    download repository finished

---
+++ src/repositories/downloads.repository.ts
@@ -0,0 +1,144 @@
+import { Db, ObjectId } from "mongodb";
+import dbConnection from "../configs/database/mongo.conn";
+import { Download } from "../models/Download";
+import { Customer } from "../models/Customer";
+import { Game } from "../models/Game";
+import { Membership } from "../models/Membership";
+
+export const getAllDownloadsRepository = async (userId: string, role: string): Promise<Download[] | null> => {
+    try {
+        const db: Db = await dbConnection();
+        const downloadsCollection = db.collection<Download>('downloads');
+
+        let downloads: Download[];
+
+        if (role === 'admin') {
+            downloads = await downloadsCollection.find().toArray();
+        } else if (role === 'customer') {
+            downloads = await downloadsCollection.find({ idCustomer: userId }).toArray();
+        } else {
+            throw new Error('Invalid role');
+        }
+
+        return downloads;
+    } catch (error) {
+        console.error('Error fetching downloads from repository: ', error);
+        return null;
+    }
+};
+
+export const createDownloadRepository = async (
+    download: Download,
+    customerId: string
+): Promise<ObjectId | null> => {
+    try {
+        const db: Db = await dbConnection();
+
+        const gamesCollection = db.collection<Game>('games');
+        const gameFilter = { _id: new ObjectId(download.idGame) };
+        const game = await gamesCollection.findOne(gameFilter);
+
+        if (!game) {
+            throw new Error(`Game with id: ${download.idGame} does not exist.`);
+        }
+
+
+        const membershipsCollection = db.collection<Membership>('memberships');
+        const membershipFilter = { _id: new ObjectId(game.membershipRequiredId) };
+        const requiredMembership = await membershipsCollection.findOne(membershipFilter);
+
+        if (!requiredMembership) {
+            throw new Error(`Membership required for the game does not exist.`);
+        }
+
+        const customersCollection = db.collection<Customer>('customers');
+        const customer = await customersCollection.findOne({ _id: new ObjectId(customerId) });
+
+        if (!customer) {
+            throw new Error(`Customer with id: ${customerId} does not exist.`);
+        }
+
+        
+        if (
+            requiredMembership.type === 'Diamond' &&
+            customer.membershipId.toString() !== requiredMembership._id.toString()
+        ) {
+            throw new Error(`Customer does not have the required membership to download the game.`);
+        }
+
+       
+        if (requiredMembership.type === 'Gold') {
+            const customerMembership = await membershipsCollection.findOne({
+                _id: new ObjectId(customer.membershipId)
+            });
+
+            if (!customerMembership) {
+                throw new Error(`Customer membership not found.`);
+            }
+
+            if (
+                customerMembership.type !== 'Diamond' &&
+                customerMembership.type !== 'Gold'
+            ) {
+                throw new Error(`Customer does not have the required membership to download the game.`);
+            }
+        }
+
+        
+        const downloadsCollection = db.collection<Download>('downloads');
+        const existingDownload = await downloadsCollection.findOne({
+            idCustomer: customerId,
+            idGame: download.idGame
+        });
+
+        if (existingDownload) {
+            throw new Error(`Game is already downloaded by the customer.`);
+        }
+       
+        const downloadToInsert: Download = {
+            idCustomer: customerId,
+            idGame: download.idGame,
+            downloadDate: new Date(),
+        };
+
+        const result = await downloadsCollection.insertOne(downloadToInsert);
+
+        return result.insertedId ? new ObjectId(result.insertedId) : null;
+    } catch (error) {
+        console.error('Error creating download in repository: ', error);
+        return null;
+    }
+};
+
+export const getDownloadByIdRepository = async (downloadId: string): Promise<Download | null> => {
+    try {
+        const db: Db = await dbConnection();
+
+        if (!ObjectId.isValid(downloadId)) {
+            throw new Error('Invalid download ID');
+        }
+
+        const filter = { _id: new ObjectId(downloadId) };
+        const download = await db.collection<Download>('downloads').findOne(filter);
+
+        return download; 
+    } catch (error) {
+        console.error('Error getting download by ID: ', error);
+        return null;
+    }
+};
+
+
+export const deleteDownloadRepository = async (downloadId: string, customerId: string): Promise<boolean> => {
+    try {
+        const db: Db = await dbConnection();
+        const downloadsCollection = db.collection<Download>('downloads');
+        
+        const deleteResult = await downloadsCollection.deleteOne({ _id: new ObjectId(downloadId), idCustomer: customerId });
+
+        return deleteResult.deletedCount === 1;
+    } catch (error) {
+        console.error('Error deleting download:', error);
+        throw error;
+    }
+};
--- src/services/download.service.ts
+++ src/services/download.service.ts
@@ -6,33 +6,37 @@ import { Download } from '../models/Download';
 import { Membership } from '../models/Membership';
 import { AuthenticatedRequest } from '../middlewares/jwt/verifyAdminJwt';
 import cache from '../middlewares/cache/nodeCacheInstance';
+import { createDownloadRepository, deleteDownloadRepository, getAllDownloadsRepository, getDownloadByIdRepository } from '../repositories/downloads.repository';
+
+
 
 //Get all downloads service
 export const getAllDownloadsService = async (req: AuthenticatedRequest): Promise<Download[] | null> => {
     try {
-        const cachedDownloads = cache.get<Download[]>('allDownloads');
-
-        if (cachedDownloads) {
-            console.log('Cache hit for all downloads!');
-            return cachedDownloads;
-        }
-
-        const db: Db = await dbConnection();
-        const downloadsCollection = db.collection<Download>('downloads');
-        
-        let downloads: Download[];
+        let downloads: Download[] | null;
 
         if (req.role === 'admin') {
-            downloads = await downloadsCollection.find().toArray();
-            cache.set('allDownloads', downloads, 300);
+            downloads = cache.get<Download[]>('adminDownloads') || null;
+            if (!downloads) {
+                downloads = await getAllDownloadsRepository(req.userId!, req.role);
+                cache.set('adminDownloads', downloads, 300);
+            } else{
+                console.log('Cache hit for all downloads!');
+            }
         } else if (req.role === 'customer') {
-            const customerId = req.userId;
-            downloads = await downloadsCollection.find({ idCustomer: customerId }).toArray();
+            const cacheKey = `customerDownloads_${req.userId}`;
+            downloads = cache.get<Download[]>(cacheKey) || null;
+            if (!downloads) {
+                downloads = await getAllDownloadsRepository(req.userId!, req.role);
+                cache.set(cacheKey, downloads, 300);
+            }else{
+                console.log('Cache hit for customer downloads!');
+            }
         } else {
             throw new Error('Invalid role');
         }
 
-        return downloads; 
+        return downloads;
     } catch (error) {
         console.error('Error fetching downloads: ', error);
         return null;
@@ -42,127 +46,30 @@ export const getAllDownloadsService = async (req: AuthenticatedRequest): Promise
 //Create a new download depending about memberships and game required kind of memberships service
 export const createDownloadService = async (
     download: Download,
-    customerId: string 
+    customerId: string
 ): Promise<ObjectId | null> => {
     try {
-        const db: Db = await dbConnection();
-
-        const gamesCollection = db.collection<Game>('games');
-        const gameFilter = { _id: new ObjectId(download.idGame) };
-        const game = await gamesCollection.findOne(gameFilter);
-
-        if (!game) {
-            throw new Error(`Game with id: ${download.idGame} does not exist.`);
-        }
-
-
-        const membershipsCollection = db.collection<Membership>('memberships');
-        const membershipFilter = { _id: new ObjectId(game.membershipRequiredId) };
-        const requiredMembership = await membershipsCollection.findOne(membershipFilter);
-
-        if (!requiredMembership) {
-            throw new Error(`Membership required for the game does not exist.`);
-        }
-
-        const customersCollection = db.collection<Customer>('customers');
-        const customer = await customersCollection.findOne({ _id: new ObjectId(customerId) });
-
-        if (!customer) {
-            throw new Error(`Customer with id: ${customerId} does not exist.`);
-        }
-
-        
-        if (
-            requiredMembership.type === 'Diamond' &&
-            customer.membershipId.toString() !== requiredMembership._id.toString()
-        ) {
-            throw new Error(`Customer does not have the required membership to download the game.`);
-        }
-
-       
-        if (requiredMembership.type === 'Gold') {
-            const customerMembership = await membershipsCollection.findOne({
-                _id: new ObjectId(customer.membershipId)
-            });
-
-            if (!customerMembership) {
-                throw new Error(`Customer membership not found.`);
-            }
-
-            if (
-                customerMembership.type !== 'Diamond' &&
-                customerMembership.type !== 'Gold'
-            ) {
-                throw new Error(`Customer does not have the required membership to download the game.`);
-            }
-        }
-
-        
-        const downloadsCollection = db.collection<Download>('downloads');
-        const existingDownload = await downloadsCollection.findOne({
-            idCustomer: customerId,
-            idGame: download.idGame
-        });
-
-        if (existingDownload) {
-            throw new Error(`Game is already downloaded by the customer.`);
-        }
-       
-        const downloadToInsert: Download = {
-            idCustomer: customerId,
-            idGame: download.idGame,
-            downloadDate: new Date(),
-        };
-
-        const result = await downloadsCollection.insertOne(downloadToInsert);
-
-        cache.del('allDownloads');
-
-        return result.insertedId ? new ObjectId(result.insertedId) : null;
+        return await createDownloadRepository(download, customerId);
     } catch (error) {
-        console.error('Error creating download: ', error);
+        console.error('Error creating download in service: ', error);
         return null;
     }
 };
-
 // Get a single download by ID  and just by an authenticated admin service
-export const getDownloadByIdService = async(downloadId: string): Promise<Game | null> => {
+export const getDownloadByIdService = async (downloadId: string): Promise<Download | null> => {
     try {
-        const db: Db = await dbConnection();
-
-        if (!ObjectId.isValid(downloadId)) {
-            throw new Error('Invalid download ID');
-        }
-
-        const filter = { _id: new ObjectId(downloadId) };
-        const game = await db.collection<Game>('downloads').findOne(filter);
-
-        return game; 
+        return await getDownloadByIdRepository(downloadId);
     } catch (error) {
         console.error('Error getting download by ID: ', error);
         return null;
     }
-}
+};
 
 // Delete an own download service
-export const deleteDownloadService = async (
-    req: AuthenticatedRequest, 
-    downloadId: string
-): Promise<boolean> => {
+export const deleteDownloadService = async (req: AuthenticatedRequest, downloadId: string): Promise<boolean> => {
     try {
-        const db: Db = await dbConnection();
-        const downloadsCollection = db.collection<Download>('downloads');
-        
         const customerId = req.userId;
-        
-        const deleteResult = await downloadsCollection.deleteOne({ _id: new ObjectId(downloadId), idCustomer: customerId });
-
-        if (deleteResult.deletedCount === 1) {
-            cache.del('allDownloads');
-            return true;
-        } else {
-            return false; 
-        }
+        return await deleteDownloadRepository(downloadId, customerId!);
     } catch (error) {
         console.error('Error deleting download:', error);
         throw error;
